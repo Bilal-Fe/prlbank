9face6aacd15d4413497a548675c6f74746d0484
Bilal <bilal.fe@gmail.com>
2021-11-17 18:32:23 -0500

    new one

--- src/test/java/dummypack/TrialOne.java
+++ src/test/java/dummypack/TrialOne.java
@@ -4,6 +4,7 @@ public class TrialOne {
     public static void main(String[] args) {
         System.out.println("hello every one");
         System.out.println("hello world!");
-
+        int c=123;
+        String name= "bilal";
     }
 }
