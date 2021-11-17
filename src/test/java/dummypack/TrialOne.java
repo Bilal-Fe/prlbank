bf4d8104dea222df6f83bf2e0099190c29790e75
Bilal <bilal.fe@gmail.com>
2021-11-17 18:18:40 -0500

    third

--- src/test/java/dummypack/TrialOne.java
+++ src/test/java/dummypack/TrialOne.java
@@ -2,6 +2,7 @@ package dummypack;
 
 public class TrialOne {
     public static void main(String[] args) {
+        System.out.println("hello every one");
         System.out.println("hello world!");
 
     }
